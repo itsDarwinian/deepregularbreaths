399ec3cbb851e20d173b366e169d1c8dea703dcd
itsDarwinian <40360696+itsDarwinian@users.noreply.github.com>
2018-07-18 13:25:31 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -64,3 +64,11 @@ var ScrollDetector = function()
 };
 
 new ScrollDetector();
+
+/*PRELOADER*/
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
