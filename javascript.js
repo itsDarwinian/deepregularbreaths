f64cd0e2d94b10e9a242daa6762d95dcbed0a1e1
Tom Ribuot <tom.ribuot@gmail.com>
2018-07-24 00:32:03 +0200

    dynamic scroll top detection for scroll-blocks

--- javascript.js
+++ javascript.js
@@ -1,108 +1,125 @@
 $(document).ready(function() {
-$('#down').on('click', function() { // Au clic sur un élément
-  console.log("t'as cliqué là, nan ?"); //debugging
-  var page = $(this).attr('href'); // Page cible
-  var speed = 750; // Durée de l'animation (en ms)
-  $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-  return false;
+  $("#down").on("click", function() {
+    // Au clic sur un élément
+    console.log("t'as cliqué là, nan ?"); //debugging
+    var page = $(this).attr("href"); // Page cible
+    var speed = 750; // Durée de l'animation (en ms)
+    $("html, body").animate({ scrollTop: $(page).offset().top }, speed); // Go
+    return false;
   });
 });
 
-var ScrollDetector = function()
-{
-    "use strict";
+var percent = 0.15;
+var blocks = document.getElementsByClassName("scroll-block");
 
-    window.addEventListener( 'scroll', function() {
-        if( window.scrollY > 100)
-        {
-            document.body.classList.add( 'scrolled' );
-        } else {
-            document.body.classList.remove( 'scrolled' );
-        }
-        if( window.scrollY > 300)
-        {
-            document.body.classList.add( 'scrolled1' );
-        } else {
-            document.body.classList.remove( 'scrolled1' );
-        }
-        if( window.scrollY > 500)
-        {
-            document.body.classList.add( 'scrolled2' );
-        } else {
-            document.body.classList.remove( 'scrolled2' );
-        }
-        if( window.scrollY > 700)
-        {
-            document.body.classList.add( 'scrolled3' );
-        } else {
-            document.body.classList.remove( 'scrolled3' );
-        }
-        if( window.scrollY > 900)
-        {
-            document.body.classList.add( 'scrolled4' );
-        } else {
-            document.body.classList.remove( 'scrolled4' );
-        }
-        if( window.scrollY > 1100)
-        {
-            document.body.classList.add( 'scrolled5' );
-        } else {
-            document.body.classList.remove( 'scrolled5' );
-        }
-        if( window.scrollY > 1300)
-        {
-            document.body.classList.add( 'scrolled6' );
-        } else {
-            document.body.classList.remove( 'scrolled6' );
-        }
-        if( window.scrollY > 1500)
-        {
-            document.body.classList.add( 'scrolled7' );
-        } else {
-            document.body.classList.remove( 'scrolled7' );
-        }
-    });
+console.log(blocks);
+
+function detector(event) {
+  for (var i = 0; i < blocks.length; i++) {
+    var block = blocks[i];
+
+    if (block.getBoundingClientRect().bottom <= window.innerHeight) {
+      if (!block.classList.contains("pulled")) block.classList.add("pulled");
+    } else {
+      if (block.classList.contains("pulled")) block.classList.remove("pulled");
+    }
+  }
+}
+
+window.addEventListener("scroll", detector);
+/*
+var ScrollDetector = function() {
+  "use strict";
+
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > 100) {
+      document.body.classList.add("scrolled");
+    } else {
+      document.body.classList.remove("scrolled");
+    }
+    if (window.scrollY > 300) {
+      document.body.classList.add("scrolled1");
+    } else {
+      document.body.classList.remove("scrolled1");
+    }
+    if (window.scrollY > 500) {
+      document.body.classList.add("scrolled2");
+    } else {
+      document.body.classList.remove("scrolled2");
+    }
+    if (window.scrollY > 700) {
+      document.body.classList.add("scrolled3");
+    } else {
+      document.body.classList.remove("scrolled3");
+    }
+    if (window.scrollY > 900) {
+      document.body.classList.add("scrolled4");
+    } else {
+      document.body.classList.remove("scrolled4");
+    }
+    if (window.scrollY > 1100) {
+      document.body.classList.add("scrolled5");
+    } else {
+      document.body.classList.remove("scrolled5");
+    }
+    if (window.scrollY > 1300) {
+      document.body.classList.add("scrolled6");
+    } else {
+      document.body.classList.remove("scrolled6");
+    }
+    if (window.scrollY > 1500) {
+      document.body.classList.add("scrolled7");
+    } else {
+      document.body.classList.remove("scrolled7");
+    }
+  });
 };
 
 new ScrollDetector();
 
+*/
+
 /*PRELOADER*/
 $(window).load(function() {
-	$(".se-pre-con").fadeOut(1500);
+  $(".se-pre-con").fadeOut(1500);
   console.log("la page a fini de charger, nan ?"); //debugging
 });
 
 /*SHOW ACTION du gform*/
 $(document).ready(function() {
-  $('#informbutton').on('click', function(){ // Au clic sur <a id="close">
+  $("#informbutton").on("click", function() {
+    // Au clic sur <a id="close">
     console.log("t'as cliqué là, nan ?"); //debugging
-    document.getElementById("gformwrapper").classList.remove( 'hidden' ); // On lui ajoute la classe hidden
-    document.getElementById("informbutton").classList.add( 'hidden' ); // On lui ajoute la classe hidden
+    document.getElementById("gformwrapper").classList.remove("hidden"); // On lui ajoute la classe hidden
+    document.getElementById("informbutton").classList.add("hidden"); // On lui ajoute la classe hidden
   });
 });
 
 /*CLOSE ACTION du gform*/
 $(document).ready(function() {
-  $('#close').on('click', function(){ // Au clic sur <a id="close">
+  $("#close").on("click", function() {
+    // Au clic sur <a id="close">
     console.log("t'as cliqué là, nan ?"); //debugging
-    document.getElementById("gformwrapper").classList.add( 'hidden' );
-    document.getElementById("informbutton").classList.remove( 'hidden' ); // On lui ajoute la classe hidden
+    document.getElementById("gformwrapper").classList.add("hidden");
+    document.getElementById("informbutton").classList.remove("hidden"); // On lui ajoute la classe hidden
   });
 });
 
 $(document).ready(function() {
-  $('.dlbutton').on('click', function(){ // Au clic sur <a id="close">
+  $(".dlbutton").on("click", function() {
+    // Au clic sur <a id="close">
     console.log("t'as cliqué là, nan ?"); //debugging
-    $(".dlbutton").addClass( 'hidden' );
-    document.getElementById("CLUF").classList.remove( 'hidden' ); // On lui ajoute la classe hidden
+    $(".dlbutton").addClass("hidden");
+    document.getElementById("CLUF").classList.remove("hidden"); // On lui ajoute la classe hidden
   });
 });
 
 $(document).ready(function() {
-  $('#CLUF a').on('click', function(){ // Au clic sur <a id="close">
+  $("#CLUF a").on("click", function() {
+    // Au clic sur <a id="close">
     console.log("t'as cliqué là, nan ?"); //debugging
-    $(".dlbutton").removeClass( 'hidden' );
-    document.getElementById("CLUF").classList.add( 'hidden' ); // On lui ajoute la classe hidden
+    $(".dlbutton").removeClass("hidden");
+    document.getElementById("CLUF").classList.add("hidden"); // On lui ajoute la classe hidden
   });
 });
 
