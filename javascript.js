daccb436366cc58bbc2ef830b94b45206d5e4fcc
Tom Ribuot <tom.ribuot@gmail.com>
2021-05-05 23:37:20 +0200

    refacto javascript code in app.js

--- javascript.js
+++ javascript.js
@@ -1,66 +1,58 @@
-$(document).ready(function() {
-$('#down').on('click', function() { // Au clic sur un élément
-  var page = $(this).attr('href'); // Page cible
-  var speed = 750; // Durée de l'animation (en ms)
-  $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-  return false;
+$(document).ready(function () {
+  $("#down").on("click", function () {
+    // Au clic sur un élément
+    var page = $(this).attr("href"); // Page cible
+    var speed = 750; // Durée de l'animation (en ms)
+    $("html, body").animate({ scrollTop: $(page).offset().top }, speed); // Go
+    return false;
   });
 });
 
-var ScrollDetector = function()
-{
-    "use strict";
+var ScrollDetector = function () {
+  "use strict";
 
-    window.addEventListener( 'scroll', function() {
-        if( window.scrollY > 100)
-        {
-            document.body.classList.add( 'scrolled' );
-        } else {
-            document.body.classList.remove( 'scrolled' );
-        }
-        if( window.scrollY > 200)
-        {
-            document.body.classList.add( 'scrolled1' );
-        } else {
-            document.body.classList.remove( 'scrolled1' );
-        }
-        if( window.scrollY > 400)
-        {
-            document.body.classList.add( 'scrolled2' );
-        } else {
-            document.body.classList.remove( 'scrolled2' );
-        }
-        if( window.scrollY > 600)
-        {
-            document.body.classList.add( 'scrolled3' );
-        } else {
-            document.body.classList.remove( 'scrolled3' );
-        }
-        if( window.scrollY > 800)
-        {
-            document.body.classList.add( 'scrolled4' );
-        } else {
-            document.body.classList.remove( 'scrolled4' );
-        }
-        if( window.scrollY > 1000)
-        {
-            document.body.classList.add( 'scrolled5' );
-        } else {
-            document.body.classList.remove( 'scrolled5' );
-        }
-        if( window.scrollY > 1200)
-        {
-            document.body.classList.add( 'scrolled6' );
-        } else {
-            document.body.classList.remove( 'scrolled6' );
-        }
-        if( window.scrollY > 1400)
-        {
-            document.body.classList.add( 'scrolled7' );
-        } else {
-            document.body.classList.remove( 'scrolled7' );
-        }
-    });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+      document.body.classList.add("scrolled");
+    } else {
+      document.body.classList.remove("scrolled");
+    }
+    if (window.scrollY > 200) {
+      document.body.classList.add("scrolled1");
+    } else {
+      document.body.classList.remove("scrolled1");
+    }
+    if (window.scrollY > 400) {
+      document.body.classList.add("scrolled2");
+    } else {
+      document.body.classList.remove("scrolled2");
+    }
+    if (window.scrollY > 600) {
+      document.body.classList.add("scrolled3");
+    } else {
+      document.body.classList.remove("scrolled3");
+    }
+    if (window.scrollY > 800) {
+      document.body.classList.add("scrolled4");
+    } else {
+      document.body.classList.remove("scrolled4");
+    }
+    if (window.scrollY > 1000) {
+      document.body.classList.add("scrolled5");
+    } else {
+      document.body.classList.remove("scrolled5");
+    }
+    if (window.scrollY > 1200) {
+      document.body.classList.add("scrolled6");
+    } else {
+      document.body.classList.remove("scrolled6");
+    }
+    if (window.scrollY > 1400) {
+      document.body.classList.add("scrolled7");
+    } else {
+      document.body.classList.remove("scrolled7");
+    }
+  });
 };
 
 new ScrollDetector();
@@ -68,26 +60,26 @@ new ScrollDetector();
 var NumberofPages = 0;
 var CurrentPage = 1;
 var zI = 1100;
-var timeLocked = false
+var timeLocked = false;
 
 /*PRELOADER*/
 //paste this code under the head tag or in a separate js file.
-	// Wait for window load
-	$(window).load(function() {
-    var elements = document.querySelectorAll('.page');
-    for(var i=0; i<elements.length; i++){
-      elements[i].style.display = 'none';
-      elements[i].onwheel = specialScroll;//listener pour le desktop scroll
-      elements[i].addEventListener("touchstart", specialTouchScrollSetup, false);
-      elements[i].addEventListener("touchend", specialTouchScroll, false);
-      NumberofPages = NumberofPages + 1;
-    }
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut(1500);
-    document.getElementById('page1').style.display = 'block';
-    //console.log('nombre de pages : '+NumberofPages)
+// Wait for window load
+$(window).load(function () {
+  var elements = document.querySelectorAll(".page");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = "none";
+    elements[i].onwheel = specialScroll; //listener pour le desktop scroll
+    elements[i].addEventListener("touchstart", specialTouchScrollSetup, false);
+    elements[i].addEventListener("touchend", specialTouchScroll, false);
+    NumberofPages = NumberofPages + 1;
+  }
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut(1500);
+  document.getElementById("page1").style.display = "block";
+  //console.log('nombre de pages : '+NumberofPages)
 
-    /*document.getElementById('nav').onmouseout = function(event) {
+  /*document.getElementById('nav').onmouseout = function(event) {
       if (CurrentPage='page1'){
         document.getElementById('nav').style.opacity=0;
         console.log('mouseout '+event.target);
@@ -100,18 +92,18 @@ var timeLocked = false
         console.log('mousein '+event.target)
       }
     }*/
-	});
+});
 
 function specialScroll(event) {
   var DestPageNb;
   //event.preventDefault();
   //console.log(event.deltaY);
 
-  if (event.deltaY>0){//Scrolling down
+  if (event.deltaY > 0) {
+    //Scrolling down
     DestPageNb = Math.min(CurrentPage + 1, NumberofPages);
     //console.log('DestPageNb :'+DestPageNb);
-  }
-  else if (event.deltaY<0) {
+  } else if (event.deltaY < 0) {
     DestPageNb = Math.max(CurrentPage - 1, 1);
     //console.log('DestPageNb :'+DestPageNb);
   }
@@ -130,46 +122,47 @@ function specialTouchScroll(event) {
   //event.preventDefault();
   //console.log(event.changedTouches[0].screenY);
 
-  if (event.changedTouches[0].screenY<TouchDeltaY){
+  if (event.changedTouches[0].screenY < TouchDeltaY) {
     DestPageNb = Math.min(CurrentPage + 1, NumberofPages);
-  }
-  else if (event.changedTouches[0].screenY>TouchDeltaY) {
+  } else if (event.changedTouches[0].screenY > TouchDeltaY) {
     DestPageNb = Math.max(CurrentPage - 1, 1);
-  }
-  else {
-    return
+  } else {
+    return;
   }
 
-  TouchDeltaY = event.changedTouches[0].screenY
+  TouchDeltaY = event.changedTouches[0].screenY;
 
   movetoPage(DestPageNb);
 }
 
 function movetoPage(nb) {
-  if (nb!=CurrentPage && !timeLocked){
+  if (nb != CurrentPage && !timeLocked) {
     //Pour l'instant, j'ai un problème si l'utilisateur décide de quitter une page et d'y revenir immédiatement.
-    var DestPage = document.getElementById('page'+nb)
-    DestPage.style.display = 'none';
+    var DestPage = document.getElementById("page" + nb);
+    DestPage.style.display = "none";
     void DestPage.offsetWidth; // trigger a DOM reflow (pour recharger les animations)
     //document.getElementById('page'+CurrentPage).style.zIndex = '1000';
-    DestPage.style.display = 'block';
+    DestPage.style.display = "block";
     zI = zI + 1;
     DestPage.style.zIndex = zI;
     //console.log('page'+nb);
 
     timeLocked = true;
-    setTimeout(function(){ timeLocked = false; }, 400);
+    const timeoutId = setTimeout(function () {
+      timeLocked = false;
+      clearTimeout(timeoutId);
+    }, 400);
 
-    var BlockerPos = 0 - 1 + (22*nb);
+    var BlockerPos = 0 - 1 + 22 * nb;
     BlockerPos = BlockerPos.toString();
-    document.getElementById('bblock').style.left = BlockerPos+'px';
+    document.getElementById("bblock").style.left = BlockerPos + "px";
     //document.getElementById('b'+nb).style.backgroundPosition='-17px';
     //document.getElementById('b'+CurrentPage).style.backgroundPosition='5px';
 
     CurrentPage = nb;
     //DestPage.onwheel = specialScroll;
     //Déplacé à l'init
-    document.getElementById('nav').style.opacity=100;
+    document.getElementById("nav").style.opacity = 100;
 
     //À mettre après la fin de l'animation (timer 800 ?)
     //document.getElementById('page'+CurrentPage).style.display = 'none';
